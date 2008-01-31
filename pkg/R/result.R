9cdc088abc99234953c47796bd1039234edd8723
webtraveller (G Z) <georg_zajko@gmx.at>
2008-01-31 14:25:25 +0000

    Result modified GZ
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vwgh@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/result.R
+++ pkg/R/result.R
@@ -1,28 +1,120 @@
 `result` <-
 function(data)
 	{	
-		x = data[["Spruch"]]
+		x <- data[["Spruch"]]
+		#x <- data
 		x <- tolower(x)
-		res = 0
-		if (x != "" ) x <- unlist(strsplit(x,"mehrbegehren"))[1]
-		#if (x != "" ) x <- unlist(strsplit(x,"?brigen"))[1]
-		pat1 <- "unbegr.ndet|abgewiesen|eingestellt|nicht (statt|stattgegeben|folge)|zur.ckgewiesen|abgelehnt|best.tigt|antrag nicht|nachzahlung auferlegt|n i c h t   s t a t t g e g e b e n|n i c h t s t a t t g e g e b e n|zur.ckwiesen|Vollstreckbarkeit hemmenden Rechtszug mehr unterliegt"
-		pat2 <- "aufgehoben|erteilt|stattgegeben|folge gegeben|(beschwerdef.hrerin|beschwerdef.hrenden Partei) Aufwendungen |wird bewilligt|bewillig \t|rechtswidrig|berichtigt|geb.hrt dem Beschwerdef.hrer|aufschiebende Wirkung zuerkannt|Bescheid dahin gehend abge?ndert"
-		pat3 <- "Vorabentscheidung|abgetreten|antrag der Beschwerdef.hrerin|in folgender Hinsicht zu h.ren|ausgesetzt"
-		if ( length(grep(pat1,x)) == 1 ) res <- 0 
+		sp <- c(0,0,0,0,0,0)
+		## 6 Grundkategorien
+
+		## Zurueckweisung
+		pat1 <- "zur.ckgewiesen"
+				
+		
+		## Abweisung 
+		pat2 <- "abgewiesen|abewiesen|abgegewiesen|nicht (statt|stattgegeben|folge)|abgelehnt"
+		
+
+		pat2X <- "n i c h t   s t a t t g e g e b e n|n i c h t s t a t t g e g e b e n"
+
+		## Aufhebung
+		pat3="aufgehoben|erteilt|folge gegeben|bewilligt|rechtswidrig|berichtigt|geb.hrt dem Beschwerdef.hrer|erlassen|angeordnet"
+
+		pat3X <- "stattgegeben"
+
+		## Einstellung
+		pat4 <- "eingestellt"
+
+		## Beschluss
+		pat5="beschlu."
+
+		## Vorabentscheidung
+		pat6="vorabentscheidung"
+		
+		## ungebr.ndet
+		## best.tigt
+		## antrag nicht
+		## nachzahlung auferlegt
+		## abgetreten
+		## antrag der beschwerdef.hrerin
+		## in folgender hinsicht zu h.ren
+		## ausgesetzt
+		## (beschwerdef.hrerin|beschwerdef.hrenden partei) aufwendungen
+		## aufschiebende Wirkung zuerkannt
+		## bescheid dahingehend abge.ndert	
+
+
+		if (length(grep(pat1,x)) >= 1 ) sp[1] <- 1
+		if (length(grep(pat2,x)) >= 1 ) sp[2] <- 1
+		if (length(grep(pat3,x)) >= 1 ) sp[3] <- 1
+		if (length(grep(pat4,x)) >= 1 ) sp[4] <- 1
+		if (length(grep(pat5,x)) >= 1 ) sp[5] <- 1
+		if (length(grep(pat6,x)) >= 1 ) sp[6] <- 1
+		
+		## Corrections
+		if (length(grep(pat2X,x)) >= 1 ) 
+		{
+			sp[2] <- 1
+		}
 		else
 		{	
-			if ( length(grep(pat2,x)) == 1 ) res <- 1
-			else
+			if (length(grep(pat3X,x)) >= 1 )
 			{
-				if ( length(grep(pat3,x)) == 1 ) res <- 2
-				else
+				sp[3] <- 1
+			}
+		}
+
+
+		#cat(res,"\n")
+		if (sum(sp) == 1 )  
+		{
+			res <- grep("1",sp)
+		}
+		else
+		{
+			if (sum(sp) == 0 )
+			{
+				res <- -10
+			}
+			else 
+			{
+				sp2 <- sp[1]*10^5+sp[2]*10^4+sp[3]*10^3+sp[4]*10^2+sp[5]*10^1+sp[6]*10^0
+				if (sum(sp) == 2 )
 				{
-					if (x == "" ) res <- -1
-					else res <- -2
+					## Mixture:
+					## teilweise Abweisung-Zurueckweisung
+					if ( sp2 == 110000 ) 
+					{
+						res <- 7
+					}
+					else
+					{
+						## teilweise Einstellung-Abweisung
+						if ( sp2 == 10100 )  
+						{
+							res <- 8
+						}
+						else
+						{
+							## teilweise Aufhebung
+							if ( sp[3] == 1 )    
+							{
+								res <- 9
+							}
+							else
+							{
+								res <- sp[1]*10^5+sp[2]*2*10^4+sp[3]*3*10^3+sp[4]*4*10^2+sp[5]*5*10^1+sp[6]*6*10^0
+							}
+						}
+					}
 				}
-			}	
-		}	
+				else 
+				{
+					res <- sp[1]*10^5+sp[2]*2*10^4+sp[3]*3*10^3+sp[4]*4*10^2+sp[5]*5*10^1+sp[6]*6*10^0
+				}
+			}
+
+		}
 	list(Result=res)
 }
 
