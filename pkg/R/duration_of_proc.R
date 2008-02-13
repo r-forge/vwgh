5b5c4be0afdb877bbc3c8a2564b8f97b6d69887e
martinkober (Martin Kober) <martin.kober@gmail.com>
2008-02-13 09:22:18 +0000

    duration modified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vwgh@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/duration_of_proc.R
+++ pkg/R/duration_of_proc.R
@@ -8,19 +8,19 @@ function(data) {
 	month_list = c("Jänner"=1, Januar=1, Feber=2, Februar=2, "März"=3, April=4,
 		Mai=5, Juni=6, Juli=7, August=8, September=9, Oktober=10, November=11, Dezember=12)
 	month_pattern = paste(names(month_list), collapse="|");
-	
-	matches = gregexpr(paste("\\d{1,2}\\.\\s+(", month_pattern, ")\\s+\\d{4}", sep=""), txt, perl=TRUE)[[1]]
+	pattern = paste("\\d{1,2}\\.\\W{1,4}(", month_pattern, ")\\D{1,4}\\d{4}", sep="")
+	matches = gregexpr(pattern, txt, perl=TRUE)[[1]]
 	
 	if (matches[1] == -1) {  
 		## No date found in 'Betreff', let's try 'Begruendung'
 		txt = data[["Begruendung"]]
-		matches = gregexpr(paste("\\d{1,2}\\.\\s+(", month_pattern, ")\\s+\\d{4}", sep=""), txt, perl=TRUE)[[1]]
+		matches = gregexpr(pattern, txt, perl=TRUE)[[1]]
 	}
 
 	dates = c()
 	for (i in 1:length(matches)) {
 		d = substr(txt, matches[i], matches[i]+attr(matches, "match.length")[i]-1)
-		d = sub("(\\d{1,2})\\.\\s+([A-Za-zäöüßÄÖÜ]+)\\s+(\\d{4})", "\\3-\\2-\\1", d, perl=TRUE)
+		d = sub("(\\d{1,2})\\.\\W{1,4}([A-Za-zäöüßÄÖÜ]+)\\D{1,4}(\\d{4})", "\\3-\\2-\\1", d, perl=TRUE)
 		dd = strsplit(d, "-")[[1]]
 		dd[2] = month_list[dd[2]]
 		dates[i] = as.Date(paste(dd, collapse="-"), "%Y-%m-%d")
